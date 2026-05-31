9096696fc60adb8b0070ea8a53a3a06f6fe001fe
jayz26v <kuronaih@gmail.com>
2026-06-01 04:07:07 +0700

    update 2

--- UAP/uapbeneran.py
+++ UAP/uapbeneran.py
@@ -2,3 +2,8 @@ class lagu:
     def __init__(self, judul, artis, genre, rating):
         self.judul = judul
         self.artis = artis 
+        self.genre = genre
+        self.rating = int(rating)
+
+def __str__(self):
+    return f"(self.judul) | {self.artis} | {self.genre} | Rating: {self.rating}"
