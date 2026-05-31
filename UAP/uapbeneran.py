aa9a82340a1e8aaf44f7632dfb161ca832b12b11
jayz26v <kuronaih@gmail.com>
2026-06-01 03:58:30 +0700

    ini baru update

--- UAP/uapbeneran.py
+++ UAP/uapbeneran.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 class lagu:
     def __init__(self, judul, artis, genre, rating):
         self.judul = judul
         self.artis = artis 
-=======
-f
->>>>>>> e8a6e259de08619909fb60fa52f4c9d2f6ceb1fc
+
